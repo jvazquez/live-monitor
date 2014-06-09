3dccc5cb44273c79e6b5221a22ff2ebe6fa3f293
Jorge Omar Vazquez <jorgeomar.vazquez@gmail.com>
2014-06-09 18:35:03 -0300

    Finished minimal functionality version

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -6,7 +6,9 @@ require.config({
 		"backbone" : "bower_components/backbone/backbone",
 		"underscore" : "bower_components/underscore/underscore",
 		"jQueryUi" : "bower_components/jqueryui/ui/minified/jquery-ui.min",
-		"socket": "http://live-monitor.pollux.box/socket.io/socket.io"
+		"socket": "http://live-monitor.pollux.box/socket.io/socket.io",
+		"json_human": "bower_components/json-human/src/json.human",
+		"crel":"bower_components/json-human/lib/crel"
 	},
 	shim : {
 		bootstrap : {
@@ -27,17 +29,33 @@ require.config({
 		},
 		socketio:{
             exports: 'io'
+        },
+        crel:{
+        	exports: 'crel'
+        },
+        json_human:{
+        	deps:['crel'],
+        	exports: 'JsonHuman'
         }
 	}
 });
-requirejs([ 'jQuery', 'bootstrap', 'backbone', 'jQueryUi', 'socket'],
-	function($, bootstrap, backbone, jqueryUi, socket) {
+requirejs([ 'jQuery', 'bootstrap', 'backbone', 'jQueryUi', 'socket', 'json_human'],
+	function($, bootstrap, backbone, jqueryUi, socket, JsonHuman) {
+	console.log('Json human is', JsonHuman);
 	var io = socket.connect("http://live-monitor.pollux.box");
 	if(io){
 		io.on('ui_log_feed', function(data){
-			console.log('Stuff is here', data);
+			$('#notification-section').append('<tr><td colspan="2" align="center">'+JsonHuman.format(data).innerHTML+'</td></tr>');
 		});
 	}else{
 		console.log('Socket not ready, sorry');
 	}
+
+	$('#perseus-feed').click(function(){
+		$('#target-feed').append($(this).data('name'));
+	});
+
+	$('#clean-feed').click(function(){
+		$('#notification-section').empty();
+	});
 });
