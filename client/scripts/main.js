5a3ab3557ed5edb4ac84316e65ae2b1e5541d488
Jorge Omar Vazquez <jorgeomar.vazquez@gmail.com>
2014-06-09 17:29:06 -0300

    Finished minimal integration

--- client/scripts/main.js
+++ client/scripts/main.js
@@ -1,27 +1,43 @@
 require.config({
-    baseUrl: "scripts/js",
-    paths: {
-        "bootstrap":"bower_components/bootstrap/dist/js/bootstrap",
-        "jQuery":"bower_components/jquery/dist/jquery.min",
-        "backbone": "bower_components/backbone/backbone",
-        "underscore": "bower_components/underscore/underscore"
-    },
-    shim:{
-    	bootstrap:{
-    		deps:["jQuery"],
-    		exports: 'Bootstrap'
-    	},
-    	jQuery:{
-    		exports: '$'
-    	},
-    	backbone:{
-    		deps:["underscore"]
-    	},
-    	underscore:{
-    		exports: '_'
-    	}
-    }
+	baseUrl : "scripts/js",
+	paths : {
+		"bootstrap" : "bower_components/bootstrap/dist/js/bootstrap",
+		"jQuery" : "bower_components/jquery/dist/jquery.min",
+		"backbone" : "bower_components/backbone/backbone",
+		"underscore" : "bower_components/underscore/underscore",
+		"jQueryUi" : "bower_components/jqueryui/ui/minified/jquery-ui.min",
+		"socket": "http://live-monitor.pollux.box/socket.io/socket.io"
+	},
+	shim : {
+		bootstrap : {
+			deps : [ "jQuery" ],
+			exports : 'Bootstrap'
+		},
+		jQuery : {
+			exports : '$'
+		},
+		jQueryUi : {
+			deps : [ "jQuery" ]
+		},
+		backbone : {
+			deps : [ "underscore" ]
+		},
+		underscore : {
+			exports : '_'
+		},
+		socketio:{
+            exports: 'io'
+        }
+	}
 });
-requirejs(['jQuery', 'bootstrap', 'backbone'],
-function($){
+requirejs([ 'jQuery', 'bootstrap', 'backbone', 'jQueryUi', 'socket'],
+	function($, bootstrap, backbone, jqueryUi, socket) {
+	var io = socket.connect("http://live-monitor.pollux.box");
+	if(io){
+		io.on('ui_log_feed', function(data){
+			console.log('Stuff is here', data);
+		});
+	}else{
+		console.log('Socket not ready, sorry');
+	}
 });
--- index.js
+++ index.js
@@ -1,41 +1,40 @@
 var env = process.env.NODE_ENV || 'development';
 var cfg = require('./config.' + env);
 module.exports = cfg;
-var http = require('http');
+var http = require('http').Server();
 var io = require('socket.io')(http);
 var sprintf = require('sprintf').sprintf;
 var redis = require('redis');
 var redis_tunnel = redis.createClient();
 
-http.createServer(function (req, res) {
-    res.writeHead(200, {'Content-Type': 'text/plain'});
-    res.end('Hello World\n');
-}).listen(cfg.port, cfg.host);
-
-redis_tunnel.on('error', function(err){
-  console.log('We had an error', err);
+redis_tunnel.on('error', function(err) {
+	console.log('We had an error', err);
 });
 
-redis_tunnel.on('ready', function(){
-  msg = sprintf("Hooked up to redis channel:%s", cfg.redis_channel);
-  console.log(msg);
-  redis_tunnel.subscribe(cfg.redis_channel, function(count, chan){
-    msg = sprintf("We have count:%s, channel:%s", count, chan);
-    console.log(msg); 
-  });
+redis_tunnel.on('ready', function() {
+	msg = sprintf("Hooked up to redis channel:%s", cfg.redis_channel);
+	console.log(msg);
+	redis_tunnel.subscribe(cfg.redis_channel, function(count, chan) {
+		msg = sprintf("We have count:%s, channel:%s", count, chan);
+		console.log(msg);
+	});
 });
 
-io.on('connection', function(socket){
-  socket.on(cfg.ui_channel, function(data){
-    redis_tunnel.subscribe(cfg.redis_channel, function(count, chann){
-      console.log('Count is', count);
-      console.log('Chan is ', chann); 
-    });
-  });
+io.on('connection', function(socket) {
+	socket.on(cfg.ui_channel, function(data) {
+		redis_tunnel.subscribe(cfg.redis_channel, function(count, chann) {
+			console.log('Count is', count);
+			console.log('Chan is ', chann);
+		});
+	});
 
-  redis_channel.on('message', function(channel, data){
-    socket.emit(cfg.ui_channel, data);
-  });  
+	redis_tunnel.on('message', function(channel, data) {
+		console.log('Sending to ', cfg.ui_channel, ' this message', data);
+		socket.emit(cfg.ui_channel, data);
+	});
 });
 
-console.log(sprintf('Server running at %s:%s/', cfg.host, cfg.port))
+http.listen(cfg.port, function() {
+	console.log('listening on *:' + cfg.port);
+});
+console.log(sprintf('Server running at %s:%s/', cfg.host, cfg.port));
