767ea62d431989503e24573630f28ac3b0f7d2c1
Jorge Vazquez <jorgeomar.vazquez@gmail.com>
2014-07-15 10:08:34 -0300

    minimal requirements achieved

--- client/scripts/js/views/home/IndexHome.js
+++ client/scripts/js/views/home/IndexHome.js
@@ -1,16 +1,18 @@
 define(['jQuery', 'backbone', 'socket', 'client_configuration',
-    'views/live_feeds/IndexFeed', 'models/live_feeds/LiveFeedModel',
+    'views/live_feeds/IndexFeed', 'views/channels/IndexChannel',
+    'models/live_feeds/LiveFeedModel',
     'models/channels/ChannelModel', 'collections/channels/ChannelsCollection',
     'text!templates/home/index.html'],
-  function($, Backbone, socket, client_configuration, indexFeed,
-    liveFeedModel, channelModel, ChannelsCollection, indexTemplate) {
-		var IndexHome = Backbone.View.extend({
+function($, Backbone, socket, client_configuration, indexFeed, IndexChannel,
+liveFeedModel, channelModel, ChannelsCollection, indexTemplate){
+
+  var IndexHome = Backbone.View.extend({
 				events:{
 					'click #perseus-feed' : 'listen_perseus_feeds',
 					'click #clean-feed': 'clean_feed_table',
-          'click #get-channels': 'get_channel_list'
+          'click #get-channels': 'get_channel_list',
+          'click #follow-channel': 'listen_channel',
 				},
-
 				delegateEvents: function(events){
 					this.cid = 'homepage-view';
 					Backbone.View.prototype.delegateEvents.call(this, events);
@@ -32,6 +34,7 @@ define(['jQuery', 'backbone', 'socket', 'client_configuration',
 					$('#notification-section').empty();
 					this.liveFeedModel.set('rawMessage', {"msg": "Awaiting new data.."});
 				},
+        /*
 				listen_perseus_feeds: function(){
 					if (this.io){
 						self = this;
@@ -43,15 +46,36 @@ define(['jQuery', 'backbone', 'socket', 'client_configuration',
 						console.log('Socket not ready, sorry');
 					}
 				},
+        */
         get_channel_list:function(){
           var channels = new channelModel();
           $.when(channels.fetch())
             .done(function(response){
-              console.log(channels.toJSON());
+              var channelView = new IndexChannel({collection: channels, el:'#channel-list'});
             })
             .fail(function(response){
               console.log("This constitutes an error", response);
             });
+        },
+        listen_channel:function(){
+          var channel_to_follow = $('#channel-list').val();
+          if(channel_to_follow)
+          {
+            if (this.io){
+						  self = this;
+              this.liveFeedModel.set('rawMessage', {"msg": "Listening perseus..."});
+						  this.io.on('ui_live_feed', function(data) {
+							  self.liveFeedModel.set('rawMessage', JSON.parse(data));
+						  });
+					  }else{
+						  console.log('Socket not ready, sorry');
+					  }         
+          }
+          else
+          {
+            console.log("You don't have any selected channel so I can't connect this to nothing");
+            alert('Check the console and just in case, retrieve the channels first...');
+          }
         }
 			});
 			return IndexHome;
